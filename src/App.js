1029bc2fabb2b1707cf943cda71f9f696ea0537c
haley bristol <hebristol@gmail.com>
2017-02-01 19:27:02 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  //event handlers here
+
+  render() {
+    return(
+      <a href="https://medium.com/javascript-scene/javascript-es6-var-let-or-const-ba58b8dcde75#.7596kavhy">ES6 Guide</a>
+    );
+  }
+}
+
+export default App;
