588e340f8c6ff4cca73df62829157fd99c1474cb
haley bristol <hebristol@gmail.com>
2017-02-01 19:48:44 -0800

    add multiple links to state

--- src/App.js
+++ src/App.js
@@ -6,14 +6,24 @@ class App extends Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      resources: [
+      "https://medium.com/javascript-scene/javascript-es6-var-let-or-const-ba58b8dcde75#.7596kavhy",
+      "https://github.com/facebookincubator/create-react-app",
+      "https://medium.freecodecamp.com/the-5-things-you-need-to-know-to-understand-react-a1dbd5d114a3#.lcjnx6ys0"
+      ]
+    };
   }
 
   //event handlers here
 
   render() {
     return(
-      <a href="https://medium.com/javascript-scene/javascript-es6-var-let-or-const-ba58b8dcde75#.7596kavhy">ES6 Guide</a>
+      <div>
+        {this.state.resources.map((resource) => {
+          return <a href="{resource}"></a>
+        })}
+      </div>
     );
   }
 }
