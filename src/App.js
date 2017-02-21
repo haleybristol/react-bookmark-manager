2ee8fddbefe1538f054690e4ad63924cecee300a
haley bristol <hebristol@gmail.com>
2017-02-20 18:53:40 -0800

    add new resource from interface

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import Subject from './Subject.js';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
+    this.addNewResource = this.addNewResource.bind(this)
 
     this.state = {resources: [
       {subject: "Functional Programming Basics", resources: [
@@ -57,37 +59,37 @@ class App extends Component {
         ]
       },
 
-      {subject: "React", resources: [
+      {subject: "Intro to React", resources: [
           {
-            title: "Facebooks Create React App Boiler Plate",
+            title: "Facebooks Create React App Boiler Plate (github)",
             url: "https://github.com/facebookincubator/create-react-app"
           },
           {
-            title: "5 Things You Need to Know About React",
+            title: "5 Things You Need to Know About React (10 min read)",
             url: "https://medium.freecodecamp.com/the-5-things-you-need-to-know-to-understand-react-a1dbd5d114a3#.lcjnx6ys0"
           },
           {
-            title: "Thinking in React",
+            title: "Thinking in React (React docs)",
             url: "https://facebook.github.io/react/docs/thinking-in-react.html"
           },
           {
-            title: "What is React",
+            title: "What is React (5 min)",
             url: "https://www.youtube.com/watch?v=JPT3bFIwJYA&list=PL55RiY5tL51oyA8euSROLjMFZbXaV7skS"
           },
           {
-            title: "Making a Single Component",
+            title: "Making a Single Component (10 min)",
             url: "https://www.youtube.com/watch?v=G40iHC-h0c0&index=4&list=PL55RiY5tL51oyA8euSROLjMFZbXaV7skS"
           },
            {
-            title: "Multiple Components",
+            title: "Multiple Components (6 min)",
             url: "https://www.youtube.com/watch?v=9wK4gHoOh1g&list=PL55RiY5tL51oyA8euSROLjMFZbXaV7skS&index=5"
           },
           {
-            title: "Outputting Dynamic Data",
+            title: "Outputting Dynamic Data (3 min)",
             url: "https://www.youtube.com/watch?v=1JZEmYwRGoU&index=6&list=PL55RiY5tL51oyA8euSROLjMFZbXaV7skS"
           },
           {
-            title: "Passing Data with Props",
+            title: "Passing Data with Props (16 min)",
             url: "https://www.youtube.com/watch?v=GIU8ekYndKw&list=PL55RiY5tL51oyA8euSROLjMFZbXaV7skS&index=7"
           },
           {
@@ -95,7 +97,7 @@ class App extends Component {
             url: "https://www.youtube.com/watch?v=fd2Cayhez58&feature=youtu.be"
           },
           {
-            title: "Composing Multiple React.js Components (6m in)",
+            title: "Composing Multiple React.js Components (6 min)",
             url: "https://www.youtube.com/watch?v=vu_rIMPROoQ"
           },
           {
@@ -103,18 +105,62 @@ class App extends Component {
             url: "https://www.youtube.com/watch?v=qh3dYM6Keuw"
           },
         ]
+      },
+      {subject: "React: Event Handlers", resources: [
+          {
+            title: "Handling Events (React docs)",
+            url: "https://facebook.github.io/react/docs/handling-events.html"
+          },
+          {
+            title: "React Stateless Functional Components: Nine Wins You Might Have Overlooked (4 min read)",
+            url: "https://hackernoon.com/react-stateless-functional-components-nine-wins-you-might-have-overlooked-997b0d933dbc#.z6yrmgmin"
+          },
+          {
+            title: "#7 Events (8 min)",
+            url: "https://www.youtube.com/watch?v=OcM__8q6p4c&list=PL55RiY5tL51oyA8euSROLjMFZbXaV7skS&index=8"
+          },
+          {
+            title: "#8 State of Components (5 min)",
+            url: "https://www.youtube.com/watch?v=JPT3bFIwJYA&list=PL55RiY5tL51oyA8euSROLjMFZbXaV7skS"
+          },
+          {
+            title: "#9 How Does ReactJS Update the DOM (5 min)",
+            url: "https://www.youtube.com/watch?v=Iw2BLUjQo1E&list=PL55RiY5tL51oyA8euSROLjMFZbXaV7skS&index=10"
+          },
+           {
+            title: "#10 Stateless Components (4 min)",
+            url: "https://www.youtube.com/watch?v=SEkfzqIgvTo&list=PL55RiY5tL51oyA8euSROLjMFZbXaV7skS&index=11"
+          },
+          {
+            title: "Understanding JavaScript's Function.prototype.bind  (13 min read)",
+            url: "https://www.youtube.com/watch?v=1JZEmYwRGoU&index=6&list=PL55RiY5tL51oyA8euSROLjMFZbXaV7skS"
+          },
+          {
+            title: "#5 JavaScript Events and Data Changes in React (7 min)",
+            url: "https://www.youtube.com/watch?v=_D1JGNidMr4&feature=youtu.be"
+          }
+        ]
       }
     ]}
   }
 
   //event handlers here
 
+  addNewResource(subject, resource) {
+    const tempState = this.state;
+    tempState.resources[subject].resources.push(resource);
+    this.setState(tempState)
+  }
   render() {
     return(
       <div>
-        <Subject items={this.state.resources[0]}/>
-        <Subject items={this.state.resources[1]}/>
-        <Subject items={this.state.resources[2]}/>
+        {
+          this.state.resources.map((resource, index) => {
+            return(
+              <Subject index={index} items={resource} addResource={this.addNewResource}/>
+            )
+          })
+        }
       </div>
     );
   }
--- src/Subject.js
+++ src/Subject.js
@@ -3,24 +3,77 @@ import React, { Component } from 'react';
 export default class Subject extends Component {
 	constructor(props) {
 		super(props)
+
+		this.handleClick = this.handleClick.bind(this);
+		this.handleTyping = this.handleTyping.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+
+		this.state = {
+			isClicked: false,
+			title: "",
+			url: "",
+		};
+
 	}
 
 	//event handlers
 
+	handleSubmit(e) {
+		e.preventDefault();
+		const newResource = {
+			title: this.state.title,
+			url: this.state.url,
+		}
+		this.props.addResource(this.props.index, newResource)
+	}
+
+	handleClick() {
+		this.setState(prevState => ({
+			isClicked: !prevState.isClicked
+		}));
+	}
+
+	handleTyping(e) {
+		this.setState({[e.target.name]: e.target.value})
+	}
+
+
+
 	render() {
 		return(
-			<div class="container">
-	  		<h2>{this.props.items.subject}</h2>
-	  		<ul>
-	  			{this.props.items.resources.map((resource) => {
-	  				return(
-	  					<li>
-	  						<a href={resource.url}>{resource.title}</a>
-	  					</li>
-	  					)
-	  			}
-	  			)}
-	  		</ul>
+			<div className="container">
+				<div className="row">
+						<div className="col-xs-4 well well-red">
+		  				<h3 onClick={this.handleClick}>{this.props.items.subject}</h3>
+						</div>
+					<div className="col-xs-8">
+			  		<ul>
+			  			{this.props.items.resources.map((resource) => {
+			  				if (this.state.isClicked) {
+				  				return(
+				  					<li>
+				  						<a href={resource.url} className="no-decoration">{resource.title}</a>
+				  					</li>
+				  				)
+			  				}
+			  			}
+			  			)}
+			  		</ul>
+			  		<form>
+			  			<label htmlFor="title">
+			  				Title&nbsp;
+				  			<input name="title" id="title" onChange={this.handleTyping} value={this.state.title}  />
+				  		</label>
+				  		<br/>
+				  		<label htmlFor="url">
+			  				URL&nbsp;
+				  			<input name="url" id="url" onChange={this.handleTyping} value={this.state.url}  />
+				  		</label>
+				  		<br/>
+				  		<button onClick={this.handleSubmit}>Add Resource</button>
+			  		</form>
+			  	</div>
+			  </div>
 	  	</div>
 		)
 	}
